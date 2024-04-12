d439eda461622339d5baebe4d8538af08502d76e
monish <monishbarse9@gmail.com>
2024-04-13 02:11:01 +0530

    image added

--- src/components/HeroCards.tsx
+++ src/components/HeroCards.tsx
@@ -14,6 +14,7 @@ import { LightBulbIcon } from "./Icons";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
 import Link from "next/link";
+import user from "../assets/user.png";
 
 export const HeroCards = () => {
   return (
@@ -41,8 +42,10 @@ export const HeroCards = () => {
       {/* Team */}
       <Card className="absolute right-[20px] top-4 w-80 flex flex-col justify-center items-center drop-shadow-xl shadow-black/10 dark:shadow-white/10">
         <CardHeader className="mt-8 flex justify-center items-center pb-2">
-          <Image
-            src="https://i.pravatar.cc/150?img=58"
+          <Image 
+            width={96}
+            height={96}
+            src={user}
             alt="user avatar"
             className="absolute grayscale-[0%] -top-12 rounded-full w-24 h-24 aspect-square object-cover"
           />
