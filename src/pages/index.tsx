1ffdd4747d1d1ed71c22c8ca48baf56f73114e4d
monish <monishbarse9@gmail.com>
2024-04-12 20:28:02 +0530

    Landing Page template added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,37 @@
-import { Button } from "@/components/ui/button";
+import { About } from "#srccomponents/About.tsx";
+import { Cta } from "#srccomponents/Cta.tsx";
+import { FAQ } from "#srccomponents/FAQ.tsx";
+import { Features } from "#srccomponents/Features.tsx";
+import { Footer } from "#srccomponents/Footer.tsx";
+import { Hero } from "#srccomponents/Hero.tsx";
+import { HowItWorks } from "#srccomponents/HowItWorks.tsx";
+import { Navbar } from "#srccomponents/Navbar.tsx";
+import { Newsletter } from "#srccomponents/Newsletter.tsx";
+import { Pricing } from "#srccomponents/Pricing.tsx";
+import { ScrollToTop } from "#srccomponents/ScrollToTop.tsx";
+import { Services } from "#srccomponents/Services.tsx";
+import { Sponsors } from "#srccomponents/Sponsors.tsx";
+import { Team } from "#srccomponents/Team.tsx";
+import { Testimonials } from "#srccomponents/Testimonials.tsx";
 
 export default function Page() {
   return (
-    <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-      Landing Page  
-    </main>
+    <>
+      <Navbar />
+      <Hero />
+      <Sponsors />
+      <About />
+      <HowItWorks />
+      <Features />
+      <Services />
+      <Cta />
+      <Testimonials />
+      <Team />
+      <Pricing />
+      <Newsletter />
+      <FAQ />
+      <Footer />
+      <ScrollToTop />
+    </>
   );
 }
