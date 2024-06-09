8185d3e5c0270c557d7475bff4483d4b3bdc6134
monish <monishbarse9@gmail.com>
2024-06-10 02:03:34 +0530

    dark theme added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,8 @@ import "#public/globals.css";
 import { AuthProvider } from "#srccontexts/auth-context.tsx";
 const inter = Inter({ subsets: ["latin"] });
 import { Toaster } from "react-hot-toast";
-import {
-  ClerkProvider,
-} from "@clerk/nextjs";
-
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 export const metadata: Metadata = {
   title: "Multi Instance Saas",
   description: "Multi Instance Saas",
@@ -20,16 +18,16 @@ export default function App({ Component, pageProps }: any) {
   return (
     <div className={inter.className}>
       <Toaster />
-      <ClerkProvider>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <ClerkProvider appearance={{ baseTheme: dark }}>
           <AuthProvider>{getLayout(<Component {...pageProps} />)}</AuthProvider>
-        </ThemeProvider>
-      </ClerkProvider>
+        </ClerkProvider>
+      </ThemeProvider>
     </div>
   );
 }
